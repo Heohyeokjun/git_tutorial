c5533987615d3c4c638e0a0df5e82382445c7c91
eejerry633 <eejerry6693@naver.com>
2023-05-06 01:27:19 +0900

    main 83%

--- 2학년 1학기/프로그래밍방법론/Project1/main.cpp
+++ 2학년 1학기/프로그래밍방법론/Project1/main.cpp
@@ -234,102 +234,26 @@ void Page::setid(){
     setpage(v[m][0],v[m][1],v[m][2],v[m][3],v[m][4],v[m][5]);
 }
 
-int Board::findunderpage(int key){
-    cout<<"under is start"<<endl;
-    countu = 0;
-    
+int Board::findabovepage(int key){
     for(int i=0; i<(int)v.size();i++){
         if(v[i][4]==key){
             m = i;
             break;
             //setpage(v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]);
+        }
+    }
+
+    for(int i=m+1; i<(int)v.size(); i++){
+        if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
+            if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
+                w.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]});
+                countw++;
+                //cout<<"under page gyupchim ok"<<endl;
+            }
         }
     }
 
     /*
-    for(int i=0; i<m; i++){
-        if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
-            if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
-                u.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]});
-                countu++;
-                cout<<"under page gyupchim ok"<<endl;
-            }
-        }
-    }
-
-    for (int i=(int)u.size()-countu; i<(int)u.size(); i++){
-            for (int h = u[i][1]; h < u[i][1]+u[i][3]; h++) {
-                for (int w = u[i][0]; w < u[i][0]+u[i][2]; w++) {
-                board[h*width + w] = u[i][5];
-                }
-            }
-            cout<<"under building"<<endl;
-        }
-    */
-
-    for (int i=0; i<m; i++){
-            for (int h = v[i][1]; h < v[i][1]+v[i][3]; h++) {
-                for (int w = v[i][0]; w < v[i][0]+v[i][2]; w++) {
-                board[h*width + w] = v[i][5];
-                }
-            }
-            cout<<"under building"<<endl;
-        }
-    
-    cout<<"under is end"<<endl;
-    return key;
-}
-
-int Board::findunderpage2(int key){
-    /*
-    for(int i=0; i<(int)v.size();i++){
-        if(v[i][4]==key){
-            m = i;
-            break;
-            //setpage(v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]);
-        }
-    }
-
-    for(int i=0; i<m; i++){
-        if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
-            if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
-                u.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]});
-                countu++;
-                cout<<"under page gyupchim ok"<<endl;
-            }
-        }
-    }
-
-    for (int i=(int)u.size()-countu; i>(int)u.size(); i++){
-            for (int h = u[i][1]; h < u[i][1]+u[i][3]; h++) {
-                for (int w = u[i][0]; w < u[i][0]+u[i][2]; w++) {
-                //page.setpage(u[i][0],u[i][1],u[i][2],u[i][3],u[i][4],u[i][5]);
-                //board[h*width + w] = page.getcontent(); //버그가능성
-                board[h*width + w] = u[i][5];
-                }
-            }
-            cout<<"under building"<<endl;
-        }
-    */
-    
-    for(int i=0; i<(int)v.size();i++){
-        if(v[i][4]==key){
-            m = i;
-            break;
-            //setpage(v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]);
-        }
-    }
-
-    for(int i=m-1; i>=0; i--){
-        if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
-            if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
-                u.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]});
-                countu++;
-                cout<<"under page gyupchim ok"<<endl;
-            }
-        }
-    }
-
     for(int m=(int)u.size()-countu+1; m<(int)u.size(); m++){        
         for(int j=(int)u.size()-countu; j<m; j++){
             if((u[m][0]<=u[j][0] && u[j][0]<u[m][0]+u[m][2]) || (u[m][0]<u[j][0]+u[j][2] && u[j][0]+u[j][2]<=u[m][0]+u[m][2]) || (u[m][0]>u[j][0] && u[m][0]+u[m][2]<u[j][0]+u[j][2])){
@@ -344,65 +268,104 @@ int Board::findunderpage2(int key){
             }
         }
     }
+    */
 
-    for(int i=0; i<(int)u.size()-countu; i++){
-        for(int j=(int)u.size()-countu; j<(int)u.size(); j++){
-            if(u[j][4] == u[i][4]){
-                u.erase(u.begin()+j);
-                countu--;
-                cout<<"under erase"<<endl;
+    for(int i=0; i<(int)w.size()-countw; i++){
+        for(int j=(int)w.size()-countw; j<(int)w.size(); j++){
+            if(w[j][4] == w[i][4]){
+                w.erase(w.begin()+j);
+                countw--;
+                cout<<"above erase"<<endl;
             }
         }
     }
 
-    for(int i=0; i<countu; i++){
-        for (int j=(int)u.size()-countu; j<(int)u.size()-i-1; j++){
-            if(u[j][4]>u[j+1][4]){
+    for(int i=0; i<countw; i++){
+        for (int j=(int)w.size()-countw; j<(int)w.size()-i-1; j++){
+            if(w[j][4]>w[j+1][4]){
                 //w.swap(j, (j+1));
                 //int imsi[6] = w[j+1];
                 //w[j+1] = w[j];
-                cout<<"underswap"<<endl;
+                //cout<<"underswap"<<endl;
                 for(int k=0; k<6; k++){
-                imsi[k] = u[j+1][k];
-                u[j+1][k] = u[j][k];
-                u[j][k] = imsi[k];
+                imsi[k] = w[j+1][k];
+                w[j+1][k] = w[j][k];
+                w[j][k] = imsi[k];
                 }
             }
         }
     }
 
-    //counting.push_back(countu);
+    for(int i=(int)w.size()-countw; i<(int)w.size(); i++){ 
+        countw = 0;
 
-    l = 0;
+        cout<<"above recursion"<<endl;    
+        findabovepage(w[i][4]);
 
-    for(int i=(int)u.size()-countu; i<(int)u.size(); i++){
-        /*if(l==0){
-            counting.push_back(0);
-        }*/       
-        countu = 0;
-        /*if(l==0){
-            count = 0;
-        }*/
-        cout<<"under recursion"<<endl;    
-        findunderpage(u[i][4]);
-        l = 1;
-        //count++; //problem3
-        //counting[]++;
     }
-    countu = 0;
+    countw = 0;
 
-    //int n;
-    //underpage building
-    /*if(l==1){
-        for(int i=0; i<u.size();i++){
-        if(u[i][4]==v[m][4]){
-            n = i;
+    for(int i=0; i<(int)v.size();i++){
+        if(v[i][4]==key){
+            m = i;
             break;
             //setpage(v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]);
+        }
+    }
+
+    for (int h = v[m][1]; h < v[m][1]+v[m][3]; h++) {
+        for (int w = v[m][0]; w < v[m][0]+v[m][2]; w++) {
+            //page.setpage(u[i][0],u[i][1],u[i][2],u[i][3],u[i][4],u[i][5]);
+            //board[h*width + w] = page.getcontent(); //버그가능성
+            board[h*width + w] = ' ';
             }
         }
+    
+    rebuildingid.push_back({v[m][0],v[m][1],v[m][2],v[m][3],v[m][4],v[m][5]});
+    building++;
 
-        for (int i=n-count; i<n; i++){
+    findunderpage(key);
+    print_board();
+
+    return key;
+}
+
+int Board::findunderpage(int key){
+    cout<<"under is start"<<endl;
+    countu = 0;
+    
+    for(int i=0; i<(int)v.size();i++){
+        if(v[i][4]==key){
+            m = i;
+            break;
+            //setpage(v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]);
+        }
+    }
+
+    /*
+    for(int i=m-1; i>=0; i--){
+        if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
+            if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
+                u.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]});
+                countu++;
+                cout<<"under page gyupchim ok"<<endl;
+            }
+        }
+    }
+    */
+        
+        for (int i=0; i<m; i++){
+            for (int h = v[i][1]; h < v[i][1]+v[i][3]; h++) {
+                for (int w = v[i][0]; w < v[i][0]+v[i][2]; w++) {
+                board[h*width + w] = v[i][5];
+                }
+            }
+            cout<<"under building"<<endl;
+        }
+        
+
+       /*
+       for (int i=(int)u.size()-1; i>=(int)u.size()-countu; i--){
             for (int h = u[i][1]; h < u[i][1]+u[i][3]; h++) {
                 for (int w = u[i][0]; w < u[i][0]+u[i][2]; w++) {
                 //page.setpage(u[i][0],u[i][1],u[i][2],u[i][3],u[i][4],u[i][5]);
@@ -410,94 +373,14 @@ int Board::findunderpage2(int key){
                 board[h*width + w] = u[i][5];
                 }
             }
-        }
-
-        count = 0;
-    }*/
-    
-    
-    if(l==1){
-        cout<<"l==1 ok"<<endl; 
-        for(int i=0; i<(int)v.size();i++){
-            if(v[i][4]==key){
-                m = i;
-                cout<<"in l==1 key checked :"<<key<<endl;
-                break;
-                //setpage(v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]);
-            }
-        }
-
-        for(int i=m-1; i>=0; i--){
-            if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
-                if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
-                    ub.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]});
-                    countub++;
-                    cout<<"in l==1 under page gyupchim ok"<<endl;
-                }
-            }
-        }
-
-        /*for(int m=(int)u.size()-countu+1; m<(int)u.size(); m++){        
-            for(int j=(int)u.size()-countu; j<m; j++){
-                if((u[m][0]<=u[j][0] && u[j][0]<u[m][0]+u[m][2]) || (u[m][0]<u[j][0]+u[j][2] && u[j][0]+u[j][2]<=u[m][0]+u[m][2]) || (u[m][0]>u[j][0] && u[m][0]+u[m][2]<u[j][0]+u[j][2])){
-                    if((u[m][1]<=u[j][1] && u[j][1]<u[m][1]+u[m][3]) || (u[m][1]<u[j][1]+u[j][3] && u[j][1]+u[j][3]<=u[m][1]+u[m][3]) || (u[m][1]>u[j][1] && u[m][1]+u[m][3]<u[j][1]+u[j][3])){
-                    
-                        //findabovepage(v[i][4]);
-                        //aboveid.push_back(w[j][4]);
-                        //count1++;
-                        u.erase(u.begin()+m);
-                        countu--;
-                    }
-                }
-            }
-        }
-
-        for(int i=0; i<(int)u.size()-countu; i++){
-            for(int j=(int)u.size()-countu; j<(int)u.size(); j++){
-                if(u[j][4] == u[i][4]){
-                    u.erase(u.begin()+j);
-                    countu--;
-                }
-            }
-        }
-
-        for(int i=0; i<countu; i++){
-            cout<<"in l==1 under swap is called"<<endl;
-            for (int j=(int)u.size()-countu; j<(int)u.size()-i-1; j++){
-                if(u[j][4]>u[j+1][4]){
-                    //w.swap(j, (j+1));
-                    //int imsi[6] = w[j+1];
-                    //w[j+1] = w[j];
-                    cout<<"in l==1 under swaping"<<endl;
-                    for(int k=0; k<6; k++){
-                    imsi[k] = u[j+1][k];
-                    u[j+1][k] = u[j][k];
-                    u[j][k] = imsi[k];
-                    }
-                }
-            }
-        }*/
-
-        for (int i=(int)ub.size(); i>(int)ub.size()-countub; i--){
-            for (int h = ub[i][1]; h < ub[i][1]+ub[i][3]; h++) {
-                for (int w = ub[i][0]; w < ub[i][0]+ub[i][2]; w++) {
-                //page.setpage(u[i][0],u[i][1],u[i][2],u[i][3],u[i][4],u[i][5]);
-                //board[h*width + w] = page.getcontent(); //버그가능성
-                board[h*width + w] = ub[i][5];
-                }
-            }
             cout<<"under building"<<endl;
         }
+        */
 
-    }
-
-    countub = 0;    
-
-    cout << "under reursion is end" << endl;
-
-    return key;
     
-
+    
+    cout<<"under is end"<<endl;
+    return key;
 }
 
 int Board::deleting(int id){
