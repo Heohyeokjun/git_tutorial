455759d876b2d8e315819fb47e3e1f697f5dd99a
eejerry633 <eejerry6693@naver.com>
2023-05-07 15:33:15 +0900

    done

--- 2학년 1학기/프로그래밍방법론/Project1/main.cpp
+++ 2학년 1학기/프로그래밍방법론/Project1/main.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 int imsi[6];
-int l;
 
 vector<vector<int>> v; 
 int m;
@@ -53,9 +52,7 @@ void manage_board(string input_file, string output_file) {
         return;
     }
 
-
     Board board = Board(num_jobs, width, height, output);
-
 
     int page_x, page_y, page_width, page_height, page_id;
     char page_content;
@@ -178,7 +175,7 @@ int main(){//int argc, char *argv[]) {
     */
 
     //input6
-    
+    /*
     Board board(21, 100, 120, fout);
     board.insert_page(12, 33, 68, 61, 29258, 'c');
     board.insert_page(8, 114, 71, 0, 18978, 'q');
@@ -201,307 +198,44 @@ int main(){//int argc, char *argv[]) {
     board.insert_page(20, 1, 40, 115, 268, 'l');  
     board.delete_page(28674);
     board.modify_position(18978, 14, 61);
+    */
+
+    //input3
     
+    Board board(30, 103, 90, fout);
+    board.insert_page(52, 12, 15, 54, 19221, 'g');
+    board.delete_page(19221);
+    board.insert_page(83, 49, 4, 2, 19861, 'a'); 
+    board.modify_position(19861, 26, 58);
+    board.modify_position(19861, 53, 27);
+    board.insert_page(5, 17, 93, 61, 13394, 'w'); 
+    board.delete_page(13394);
+    board.modify_position(19861, 2, 55);
+    board.insert_page(84, 6, 5, 82, 8558, 'u');
+    board.modify_position(8558, 34, 2);
+    board.delete_page(8558);
+    board.delete_page(19861);
+    board.insert_page(2, 70, 94, 19, 21491, 'x');
+    board.modify_position(21491, 3, 46);
+    board.insert_page(24, 64, 27, 20, 6905, 'u');
+    board.modify_position(21491, 0, 71);
+    board.insert_page(23, 1, 57, 77, 26431, 'b');
+    board.insert_page(83, 19, 4, 47, 18874, 'w');
+    board.modify_content(26431, 'w');
+    board.insert_page(3, 24, 83, 27, 11430, 'j'); 
+    board.delete_page(26431);
+    board.insert_page(36, 25, 5, 62, 30233, 'a');
+    board.modify_position(18874, 97, 15);
+    board.insert_page(11, 23, 43, 21, 13481, 'g'); 
+    board.modify_content(21491, 'f');
+    board.modify_position(21491, 6, 16);
+    board.insert_page(19, 39, 73, 37, 27415, 'f'); 
+    board.modify_position(21491, 7, 20);
+    board.modify_position(11430, 12, 62);
+    board.modify_position(13481, 36, 6);
 
     fout.close();
 
     return 0;
     
-}
-
-void Board::takeid(int key){
-    for(int i=0; i<(int)v.size(); i++){
-        if(v[i][4]==key){
-            m = i;
-            break;
-        }
-    }
-}
-
-void Page::setid(){
-    setpage(v[m][0],v[m][1],v[m][2],v[m][3],v[m][4],v[m][5]);
-}
-
-/*
-bool gyupchim(int m){
-    if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
-        if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
-                return true;
-            }
-    }
-
-    else{return false;}
-}*/
-
-int Board::findabovepage(int key){
-    
-    takeid(key);
-
-    for(int i=m+1; i<(int)v.size(); i++){
-        if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
-            if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
-                w.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5]});
-                countw++;
-                cout<<"above page gyupchim ok"<<endl;
-            }
-        }
-    }
-
-    /*
-    for(int m=(int)u.size()-countu+1; m<(int)u.size(); m++){        
-        for(int j=(int)u.size()-countu; j<m; j++){
-            if((u[m][0]<=u[j][0] && u[j][0]<u[m][0]+u[m][2]) || (u[m][0]<u[j][0]+u[j][2] && u[j][0]+u[j][2]<=u[m][0]+u[m][2]) || (u[m][0]>u[j][0] && u[m][0]+u[m][2]<u[j][0]+u[j][2])){
-                if((u[m][1]<=u[j][1] && u[j][1]<u[m][1]+u[m][3]) || (u[m][1]<u[j][1]+u[j][3] && u[j][1]+u[j][3]<=u[m][1]+u[m][3]) || (u[m][1]>u[j][1] && u[m][1]+u[m][3]<u[j][1]+u[j][3])){
-                    
-                    //findabovepage(v[i][4]);
-                    //aboveid.push_back(w[j][4]);
-                    //count1++;
-                    u.erase(u.begin()+m);
-                    countu--;
-                }
-            }
-        }
-    }
-    */
-
-/*
-    for(int i=0; i<(int)w.size()-countw; i++){
-        for(int j=(int)w.size()-countw; j<(int)w.size(); j++){
-            if(w[j][4] == w[i][4]){
-                w.erase(w.begin()+j);
-                countw--;
-                cout<<"above erase"<<endl;
-            }
-        }
-    }
-    */
-
-    for(int i=0; i<countw; i++){
-        for (int j=(int)w.size()-countw; j<(int)w.size()-i-1; j++){
-            if(w[j][4]>w[j+1][4]){
-                for(int k=0; k<6; k++){
-                imsi[k] = w[j+1][k];
-                w[j+1][k] = w[j][k];
-                w[j][k] = imsi[k];
-                }
-            }
-        }
-    }
-
-    int c = (int)w.size(); //error possible
-    for(int i=c-countw; i<c; i++){ 
-        countw = 0;
-
-        cout<<"above recursion"<<endl;    
-        findabovepage(w[i][4]);
-
-    }
-    countw = 0;
-
-    for(int i=0; i<(int)ubb.size(); i++){
-        if(ubb[i][4]==key){
-            return -1;
-        }
-    }  
-
-    takeid(key);
-
-    for (int h = v[m][1]; h < v[m][1]+v[m][3]; h++) {
-        for (int w = v[m][0]; w < v[m][0]+v[m][2]; w++) {
-            board[h*width + w] = ' ';
-        }
-    }
-    
-    rebuildingid.push_back({v[m][0],v[m][1],v[m][2],v[m][3],v[m][4],v[m][5]});
-    building++;
-
-    findunderpage(key);
-    print_board();
-
-    return key;
-}
-
-int Board::findunderpage(int key){
-    cout<<"under is start"<<endl;
-    countu = 0;
-
-    takeid(key);
-
-    ubb.push_back({v[m][0],v[m][1],v[m][2],v[m][3],v[m][4],v[m][5],m});
-
-    cout<<v[m][5]<<endl;
-    
-    for(int i=0; i<m; i++){
-        if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
-            if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
-                u.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5],i});
-                countu++;
-                cout<<"under page gyupchim ok 1"<<endl;
-            }
-        }
-    }
-    
-    for(int j=0; j<(int)u.size(); j++){
-        cout<<"In u ";
-        cout<<u[j][5]<<endl;
-    }
-
-    int usize=(int)u.size();
-    for(int i=0; i<usize; i++){
-        for(int j=u[i][6]+1; j<(int)v.size(); j++){
-            //cout<<j<<endl;
-            //cout<<m<<endl;
-            if(j!=m){
-                if((u[i][0]<=v[j][0] && v[j][0]<u[i][0]+u[i][2]) || (u[i][0]<v[j][0]+v[j][2] && v[j][0]+v[j][2]<=u[i][0]+u[i][2]) || (u[i][0]>v[j][0] && u[i][0]+u[i][2]<v[j][0]+v[j][2])){
-                    if((u[i][1]<=v[j][1] && v[j][1]<u[i][1]+u[i][3]) || (u[i][1]<v[j][1]+v[j][3] && v[j][1]+v[j][3]<=u[i][1]+u[i][3]) || (u[i][1]>v[j][1] && u[i][1]+u[i][3]<v[j][1]+v[j][3])){
-                        u.push_back({v[j][0],v[j][1],v[j][2],v[j][3],v[j][4],v[j][5],j}); 
-                        countu++;
-                        cout<<"all above ok 2"<<endl;
-                    }
-                }
-            }
-        }
-    }
-    //cout<<"??"<<endl;
-    //problem: key의 above까지도 u벡터에 저장되버림. 
-    //따라서 key의 above를 예외처리해야함.
-    //의문:key above는 (모든 under의 above의 합집합)의 교집합인가? -> 아니다.
-
-    //countub=0;
-    //같은 원소 빼주기
-
-    /*
-    for(int j=0; j<(int)u.size(); j++){
-        cout<<"In u ";
-        cout<<u[j][5]<<endl;
-    }
-    */
-
-    for(int i=0; i<(int)u.size()-1; i++){
-        cout<<"next i"<<endl;
-        for(int j=i+1; j<(int)u.size(); j++){
-            cout<<"next j"<<endl;
-            if(u[i][6]==u[j][6]){
-                do{
-                cout <<u[j][5] << endl;
-                u.erase(u.begin()+j);
-                cout<<"same elements ok 3"<<endl;
-                //countub++
-                if(i+1==(int)u.size()){
-                    break;
-                }
-                }while(u[i][6]==u[j][6] && j<(int)u.size());
-            }     
-        }
-    }
-
-    countub = 0;
-    for(int i=m+1; i<(int)v.size(); i++){
-        if((v[m][0]<=v[i][0] && v[i][0]<v[m][0]+v[m][2]) || (v[m][0]<v[i][0]+v[i][2] && v[i][0]+v[i][2]<=v[m][0]+v[m][2]) || (v[m][0]>v[i][0] && v[m][0]+v[m][2]<v[i][0]+v[i][2])){
-            if((v[m][1]<=v[i][1] && v[i][1]<v[m][1]+v[m][3]) || (v[m][1]<v[i][1]+v[i][3] && v[i][1]+v[i][3]<=v[m][1]+v[m][3]) || (v[m][1]>v[i][1] && v[m][1]+v[m][3]<v[i][1]+v[i][3])){                
-                ub.push_back({v[i][0],v[i][1],v[i][2],v[i][3],v[i][4],v[i][5],i});
-                countub++;
-            }
-        }
-        cout<<"In findunder, above page gyupchim ok 4"<<endl;
-    }
-
-    for(int i=0; i<(int)ub.size(); i++){
-        for(int j=0; j<(int)u.size(); j++){
-            if(ub[i][6]==u[j][6]){
-                u.erase(u.begin()+j);
-            }
-        }
-        cout<<"find key above=all above and erase ok 5"<<endl;
-    } //error possible low
-
-    for(int i=0; i<(int)ubb.size(); i++){
-        for(int j=0; j<(int)u.size(); j++){
-            if(ubb[i][6]==u[j][6]){
-                u.erase(u.begin()+j);
-            }
-        }
-        cout<<"find key above=all above and erase ok 5.1"<<endl;
-    } //error possible low
-
-    //sorting하기
-    for(int i=0; i<(int)u.size(); i++){
-        for (int j=0; j<(int)u.size()-i-1; j++){
-            if(u[j][6]>u[j+1][6]){
-                for(int k=0; k<7; k++){
-                imsi[k] = u[j+1][k];
-                u[j+1][k] = u[j][k];
-                u[j][k] = imsi[k];
-                }
-            }
-        cout<< "In findunder, sorting ok 6" << endl;   
-        }
-    }
-
-    //출력하기
-    for (int i=0; i<(int)u.size(); i++){
-        for (int h = u[i][1]; h < u[i][1]+u[i][3]; h++) {
-            for (int w = u[i][0]; w < u[i][0]+u[i][2]; w++) {
-                board[h*width + w] = u[i][5];
-            }
-        }
-            cout<<"under building 7"<<endl;
-    }
-
-    u.clear();
-    ub.clear();
-
-        /*
-        for (int i=0; i<m; i++){
-            for (int h = v[i][1]; h < v[i][1]+v[i][3]; h++) {
-                for (int w = v[i][0]; w < v[i][0]+v[i][2]; w++) {
-                board[h*width + w] = v[i][5];
-                }
-            }
-            cout<<"under building"<<endl;
-        }
-        */
-
-       /*
-       for (int i=(int)u.size()-1; i>=(int)u.size()-countu; i--){
-            for (int h = u[i][1]; h < u[i][1]+u[i][3]; h++) {
-                for (int w = u[i][0]; w < u[i][0]+u[i][2]; w++) {
-                //page.setpage(u[i][0],u[i][1],u[i][2],u[i][3],u[i][4],u[i][5]);
-                //board[h*width + w] = page.getcontent(); //버그가능성
-                board[h*width + w] = u[i][5];
-                }
-            }
-            cout<<"under building"<<endl;
-        }
-        */
-
-    cout<<"under is end"<<endl;
-    return key;
-}
-
-int Board::deleting(int id){
-    
-    findabovepage(id); 
-    w.clear();
-    ubb.clear();
-    //cout << "print above deleted id"<<endl;
-    return id;        
-    
-    
-}
-
-void Board::rebuilding(){
-    for(int i=(int)rebuildingid.size()-2; i>=(int)rebuildingid.size()-building; i--){   
-        for (int h = rebuildingid[i][1]; h < rebuildingid[i][1]+rebuildingid[i][3]; h++) {
-            for (int w = rebuildingid[i][0]; w < rebuildingid[i][0]+rebuildingid[i][2]; w++) {
-                board[h*width + w] = rebuildingid[i][5];
-            }
-        }
-        print_board();
-        cout<< "print rebuilding page" << endl;
-    }
-
-    rebuildingid.clear();
-    building = 0;
-    //벡터초기화 필요 // ok
 }
